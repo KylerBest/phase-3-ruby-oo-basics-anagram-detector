cf29b26b6bc528511bd7345523bb0d27e4e07934
Kyler Best <kyler.b@live.com>
2022-02-22 15:26:43 -0600

    commit tree 28ca0f772e5fafd72596c4301e4ce30adcac210b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def is_anagram(new_word)
+        self.word.split('').sort == new_word.split('').sort
+    end
+
+    def match(arr)
+        arr.filter {|new_word| self.is_anagram(new_word)}
+    end
+end
